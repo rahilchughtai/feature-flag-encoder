7e87cdf21a15e8b9eaae444147b78f23ac9a1bd8
Rahil CHUGHTAI <rahil.chughtai@amadeus.com>
2024-09-02 22:45:21 +0200

    remove encoder

--- src/utils/encoder.ts
+++ src/utils/encoder.ts
@@ -8,14 +8,3 @@ export function encodeFlags(featureFlags: FeatureFlagChipData[]): string {
     });
     return btoa(JSON.stringify(payload));
 }
-
-export function generateURL(tenant: string, environment: string, encoding:string): string {
-    if (tenant === '' || environment === '') { return '' }
-    return `https://shop-uat.${tenant}/${environment}/availability/?trace=true`
-
-}
-
-//https://shop-uat.lufthansa.com/lh/booking-ci/availability/0
-
-
-//https://shop.uat.lufthansa.com/lh/booking-ci/availability/0?flagsOverride=eyJGU0QtMzAyMCI6InRydWUiLCJBQ0YtMTAyIjoidHJ1ZSJ9?trace=true
