9e93d282310cc5613a0714f188749b3189690b04
Rahil CHUGHTAI <rahil.chughtai@amadeus.com>
2024-09-06 13:49:50 +0200

    encoder update critical

--- src/utils/encoder.ts
+++ src/utils/encoder.ts
@@ -1,12 +1,12 @@
 import { FeatureFlagChipData } from "../models/featureFlagModel";
 
 export function encodeFlags(featureFlags: FeatureFlagChipData[]): string {
-    const payload: Record<string, string> = {}
+    const payload: Record<string, boolean> = {}
     if(featureFlags.length === 0) {
         return 'No flags to encode';
     }
     featureFlags.forEach(flag => {
-        const isFFEnabled = flag.enabled.valueOf().toString()
+        const isFFEnabled = flag.enabled.valueOf()
         payload[flag.label] = isFFEnabled;
     });
     return btoa(JSON.stringify(payload));
